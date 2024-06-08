ed56d318552e0986b610905d6ee4029e608fbff2
Muhammad Mohad <l230784@lhr.nu.edu.pk>
2024-06-08 07:54:48 +0500

    Adjustments

--- Lab Manuals/Lab Manual 6/Driver.cpp
+++ Lab Manuals/Lab Manual 6/Driver.cpp
@@ -4,69 +4,69 @@ using namespace std;
 
 int main()
 {
-    ComplexNumber c1;
+    // ComplexNumber c1;
 
 
-    c1.Input();
+    // c1.Input();
 
-    cout << "\nc1 is : ";
-    c1.Output();
-
-
-    ComplexNumber* cPtr = new ComplexNumber(3, 5);
-
-    cout << "\ncPtr is : ";
-    cPtr->Output();
-
-    delete cPtr;
-
-    ComplexNumber arr[5];
-
-    cout << "\nArray on Stack:\n\n";
-
-    for(int i = 0; i < 5; i++)
-    {
-        cout << "\nEnter values for arr[" << i << "] :\n";
-        arr[i].Input();
-    }
-
-    for(int i = 0; i < 5; i++)
-    { 
-        cout << "\narr[" << i << "] is : ";
-        arr[i].Output();
-
-        cout << "Magnitude of arr[" << i << "] is : " << arr[i].Magnitude() << endl;
-    }
-
-    int size = 0;
-
-    cout << "\nEnter the size of the dynamically allocated array of complex numbers : ";
-    cin >> size;
-
-    ComplexNumber* ptr = new ComplexNumber[size];
-
-    for(int i = 0; i < size; i++)
-    {
-        cout << "\nEnter values for ptr[" << i << "] :\n";
-        ptr[i].Input();
-    }
-
-    for(int i = 0; i < size; i++)
-    {
-        cout << "\nptr[" << i << "] is : ";
-        ptr[i].Output();
-
-        cout << "Magnitude of ptr[" << i << "] is : " << ptr[i].Magnitude() << endl;
-    }
-
-    delete[] ptr;
+    // cout << "\nc1 is : ";
+    // c1.Output();
 
 
+    // ComplexNumber* cPtr = new ComplexNumber(3, 5);
 
-    // ComplexNumber c1(56, -3), c2(8, -6);
+    // cout << "\ncPtr is : ";
+    // cPtr->Output();
 
-    // ComplexNumber sum = c1.Add(c2);
-    // sum.Output();
+    // delete cPtr;
+
+    // ComplexNumber arr[5];
+
+    // cout << "\nArray on Stack:\n\n";
+
+    // for(int i = 0; i < 5; i++)
+    // {
+    //     cout << "\nEnter values for arr[" << i << "] :\n";
+    //     arr[i].Input();
+    // }
+
+    // for(int i = 0; i < 5; i++)
+    // { 
+    //     cout << "\narr[" << i << "] is : ";
+    //     arr[i].Output();
+
+    //     cout << "Magnitude of arr[" << i << "] is : " << arr[i].Magnitude() << endl;
+    // }
+
+    // int size = 0;
+
+    // cout << "\nEnter the size of the dynamically allocated array of complex numbers : ";
+    // cin >> size;
+
+    // ComplexNumber* ptr = new ComplexNumber[size];
+
+    // for(int i = 0; i < size; i++)
+    // {
+    //     cout << "\nEnter values for ptr[" << i << "] :\n";
+    //     ptr[i].Input();
+    // }
+
+    // for(int i = 0; i < size; i++)
+    // {
+    //     cout << "\nptr[" << i << "] is : ";
+    //     ptr[i].Output();
+
+    //     cout << "Magnitude of ptr[" << i << "] is : " << ptr[i].Magnitude() << endl;
+    // }
+
+    // delete[] ptr;
+
+
+
+    ComplexNumber c1(56, -3), c2(8, -6);
+
+    ComplexNumber sum = c1.Add(c2);
+    sum.Output();
 
 
 
